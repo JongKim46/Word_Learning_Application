ffe4b099b85a2e861282a9412d37cf8267688b45
Jong Kim <j-kim@fores.jp>
2023-07-18 18:59:53 +0900

    7/18 API対応中

--- app/src/main/java/com/example/word_learning_application/SecondChooseActivity.kt
+++ app/src/main/java/com/example/word_learning_application/SecondChooseActivity.kt
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_second_choose.*
 
+
 class SecondChooseActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,23 +18,24 @@ class SecondChooseActivity : AppCompatActivity() {
         wordLearning.putExtra("wordLever", wordLevle)
         wordLearning.putExtra("screenCount", 0)
         var wordLists = arrayListOf<WordResult>()
-        wordLists.add(WordResult(1,"学習","がくしゅう","あ","え",0))
-        wordLists.add(WordResult(2,"単語","たんご","が","き",0))
 
         //3秒選択
         time_3.setOnClickListener {
+            wordLists = wordLevle?.let { it1 -> selectWord(it1) }!!
             wordLearning.putExtra("chooseTime", 3)
             wordLearning.putParcelableArrayListExtra("wordLists", wordLists)
             startActivity(wordLearning)
         }
         //5秒選択
         time_5.setOnClickListener {
+            wordLists = wordLevle?.let { it1 -> selectWord(it1) }!!
             wordLearning.putExtra("chooseTime", 5)
             wordLearning.putParcelableArrayListExtra("wordLists", wordLists)
             startActivity(wordLearning)
         }
         //10秒選択
         time_10.setOnClickListener {
+            wordLists = wordLevle?.let { it1 -> selectWord(it1) }!!
             wordLearning.putExtra("chooseTime", 10)
             wordLearning.putParcelableArrayListExtra("wordLists", wordLists)
             startActivity(wordLearning)
@@ -42,5 +44,9 @@ class SecondChooseActivity : AppCompatActivity() {
         cancel_button.setOnClickListener {
             finish()
         }
+
+    }
+    fun selectWord (wordLevel: String): ArrayList<WordResult>{
+        return Word_Select().wordSelect(wordLevel)
     }
 }
---
+++ app/src/main/java/com/example/word_learning_application/Word_Select.kt
@@ -0,0 +1,47 @@
+package com.example.word_learning_application
+
+import java.sql.*
+
+class Word_Select {
+
+    fun wordSelect(wordLevel: String): ArrayList<WordResult> {
+        var conn: Connection? = null
+        var stmt: Statement? = null
+        var rs: ResultSet? = null
+        var wordResult = ArrayList<WordResult>()
+        println("connect reid")
+        try {
+            conn = DriverManager.getConnection(
+                "jdbc:mysql://localhost:3309/word_learning",
+                "root",
+                "root"
+            )
+            println("connect ok")
+            var sql: String = "SELECT DISTINCT WORD_ID,WORD_KANJI,WORD_HURIGANA,WORD_HURIGANA_TEST1,WORD_HURIGANA_TEST2 FROM word_table"
+
+            stmt = conn.createStatement()
+            rs = stmt.executeQuery(sql)
+
+            while (rs.next()) {
+                wordResult.add(
+                    WordResult(
+                        rs.getInt("WORD_ID"),
+                        rs.getString("WORD_KANJI"),
+                        rs.getString("WORD_HURIGANA"),
+                        rs.getString("WORD_HURIGANA_TEST1"),
+                        rs.getString("WORD_HURIGANA_TEST2"),
+                        0
+                    )
+                )
+            }
+
+        } catch (e: Exception) {
+            println(e)
+        } finally {
+            rs?.close()
+            stmt?.close()
+            conn?.close()
+        }
+        return wordResult
+    }
+}
