3a42f3c98884f180593ff8b396410eda288b690a
Jong Kim <j-kim@fores.jp>
2023-07-19 11:45:04 +0900

    7/19 API対応中

--- app/src/main/java/com/example/word_learning_application/Word_Select.kt
+++ app/src/main/java/com/example/word_learning_application/Word_Select.kt
@@ -1,5 +1,6 @@
 package com.example.word_learning_application
 
+import android.os.StrictMode
 import java.sql.*
 
 class Word_Select {
@@ -9,15 +10,23 @@ class Word_Select {
         var stmt: Statement? = null
         var rs: ResultSet? = null
         var wordResult = ArrayList<WordResult>()
+        var url = "jdbc:mysql://localhost:3306/word_learning?useUnicode=yes&characterEncoding=UTF-8&useSSL=false&allowPublicKeyRetrieval=true&enabledTLSProtocols=TLSv1.2"
+        var user = "root"
+        var password = "root"
+
         println("connect reid")
+        println("wordLevel : " + wordLevel)
+
+        val policy = StrictMode.ThreadPolicy.Builder().permitAll().build()
+        StrictMode.setThreadPolicy(policy)
         try {
+            Class.forName("com.mysql.cj.jdbc.Driver")
             conn = DriverManager.getConnection(
-                "jdbc:mysql://localhost:3306/word_learning",
-                "root",
-                "root"
+                url, user, password
             )
             println("connect ok")
-            var sql: String = "SELECT DISTINCT WORD_ID,WORD_KANJI,WORD_HURIGANA,WORD_HURIGANA_TEST1,WORD_HURIGANA_TEST2 FROM word_table"
+            var sql: String =
+                "SELECT DISTINCT WORD_ID,WORD_KANJI,WORD_HURIGANA,WORD_HURIGANA_TEST1,WORD_HURIGANA_TEST2 FROM word_table"
 
             stmt = conn.createStatement()
             rs = stmt.executeQuery(sql)
