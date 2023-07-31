c823d5688d03dc769e991616cd3e3c7023b41a8f
Jong Kim <j-kim@fores.jp>
2023-07-31 17:27:36 +0900

    7/21 SpringAPI作成。

--- app/src/main/java/com/Http/word_learning_application/SelectWordListAPI.kt
+++ app/src/main/java/com/Http/word_learning_application/SelectWordListAPI.kt
@@ -30,6 +30,43 @@ class SelectWordListAPI {
                         )
                     )
                 }
+
+                wordSelectReturn.addAll(wordSelectLists)
+                Log.d("body1", wordSelectReturn.toString())
+                wordSelectReturn = wordSelectLists
+                Log.d("body2", wordSelectReturn.toString())
+            }
+
+            override fun onFailure(call: Call<ArrayList<WordResult>>, t: Throwable) {
+                t.printStackTrace()
+            }
+        })
+        Log.d("return wordSelectReturn", wordSelectReturn.toString())
+        return wordSelectReturn
+    }
+
+
+    fun wordSelectJson(wordLevel: String): ArrayList<WordResult> {
+        var baseurl = HttpSpringHelp.getRetrofit().create(WordAPI::class.java)
+        baseurl?.getWordList(wordLevel)?.enqueue(object : Callback<ArrayList<WordResult>> {
+            override fun onResponse(
+                call: Call<ArrayList<WordResult>>,
+                response: Response<ArrayList<WordResult>>
+            ) {
+                var bodyList = response.body()!!
+                for (body in bodyList) {
+                    wordSelectLists.add(
+                        WordResult(
+                            body.word_id,
+                            body.word_kanji,
+                            body.hurigana,
+                            body.Test1,
+                            body.Test2,
+                            0
+                        )
+                    )
+                }
+
                 Log.d("body", wordSelectLists.toString())
 
             }
@@ -39,37 +76,5 @@ class SelectWordListAPI {
             }
         })
         return wordSelectLists
-    }
-
-    fun wordSelectJson(wordLevel: String): ArrayList<WordResult> {
-        var baseurl = HttpSpringHelp.getRetrofit().create(WordAPI::class.java)
-        baseurl?.getWordList(wordLevel)?.enqueue(object : Callback<ArrayList<WordResult>> {
-            override fun onResponse(
-                call: Call<ArrayList<WordResult>>,
-                response: Response<ArrayList<WordResult>>
-            ) {
-                var dataList = response.body()
-                for (index in 0 until dataList!!.size) {
-                    Log.d("JsonbodyFor", dataList[index].toString())
-                    wordSelectLists.add(
-                        WordResult(
-                            dataList[index].word_id,
-                            dataList[index].word_kanji,
-                            dataList[index].hurigana,
-                            dataList[index].Test1,
-                            dataList[index].Test2,
-                            0
-                        )
-                    )
-                }
-                Log.d("Jsonbody1", wordSelectLists.toString())
-                wordSelectReturn = wordSelectLists.toCollection(mutableListOf()) as ArrayList<WordResult>
-            }
-            override fun onFailure(call: Call<ArrayList<WordResult>>, t: Throwable) {
-                t.printStackTrace()
-            }
-        })
-        Log.d("Jsonbody2", wordSelectLists.toString())
-        return wordSelectReturn
     }
 }
